fd227b0509f620e5c70b55d096eaf2ab4e3b2968
Dr. Bleep <drbleep@bleeplabs.com>
2020-08-21 19:13:26 -0500

    Create Class_3_-_2_singe_pot_two_osc.ino

---
+++ Class_3/Class_3_-_2_singe_pot_two_osc/Class_3_-_2_singe_pot_two_osc.ino
@@ -0,0 +1,99 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthWaveform       waveform2;      //xy=122.50001525878906,196.66668891906738
+AudioSynthWaveform       waveform1;      //xy=130.8333282470703,144.1666774749756
+AudioMixer4              mixer1;         //xy=329.16673278808594,178.33335876464844
+AudioOutputI2S           i2s1;           //xy=497.8334732055664,167.16669368743896
+AudioConnection          patchCord1(waveform2, 0, mixer1, 1);
+AudioConnection          patchCord2(waveform1, 0, mixer1, 0);
+AudioConnection          patchCord3(mixer1, 0, i2s1, 0);
+AudioControlSGTL5000     sgtl5000_1;     //xy=331.66670989990234,76.83334732055664
+// GUItool: end automatically generated code
+
+
+//Then we have our variable declarations like before
+unsigned long cm;
+unsigned long prev[8];
+float freq[8];
+
+void setup() {
+  //there's a lot we need to do in setup now but some of it is just copy paste.
+  // This first group should only be done in setup
+
+  //How much RAM to set aside for the audio library to use.
+  // The audio library uses blocks of a set size so this is not a percentage or kilobytes, just a kind of arbitrary number.
+  // On our Teensy 3.2 we can go up to about 200 but that won't leave any RAM for anyone else.
+  // It's usually the delay and reverb that hog it.
+  AudioMemory(10);
+
+  sgtl5000_1.enable(); //Turn the adapter board on
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN); //Tell it what input we want to use. Not necessary yet but good to have
+  sgtl5000_1.lineInLevel(13); //The volume of the input. Again we'll get to this later
+
+  //Output volume. Goes from 0.0 to 1.0 but a fully loud signal will clip over .8 or so.
+  // For headphones it's pretty loud at .4
+  // There are lots of places we can change the final volume level. This one you set once and leave alone.
+  sgtl5000_1.volume(0.8);
+
+  //This next group can be done anywhere in the code but we want to start things with these
+  // values and change some of them in the loop.
+
+  //Notice we start by writing the object we want, then a period, then the function
+  // begin(volume from 0.0-1.0 , frequency , shape of oscillator)
+  // See the tool for more info https://www.pjrc.com/teensy/gui/?info=AudioSynthWaveform
+  waveform1.begin(1, 220.0, WAVEFORM_SINE);
+  waveform2.begin(1, 440.0, WAVEFORM_SINE);
+
+  //The mixer has four inputs we can change the volume of
+  // gain.(channel from 0 to 3, gain from 0.0 to a large number)
+  // A gain of 1 means the output is the same as the input.
+  // .5 would be half volume and 2 would be double
+  // Since we have two oscillators coming in that are already "1" We should take them down by half so we don't clip.
+  // If you go over "1" The top or bottom of the wave is just slammed against a wall
+  mixer1.gain(0, .5);
+  mixer1.gain(1, .5);
+  mixer1.gain(2, 0);
+  mixer1.gain(3, 0);
+
+  //Then we do the stuff we've done before.
+  analogWriteResolution(12); //PWM and A14 DAC output will be 0-4095. This has no effect on the 16 bit in/out of the audio adapter
+  analogReadResolution(12); //AnalogReads will return 0-4095
+  analogReadAveraging(64);
+
+}
+
+void loop() {
+  cm = millis();
+
+  //We don't have to do anything in the loop since the audio library will jut keep doing what we told it in the setup
+
+  if (cm - prev[1] > 20) {
+    prev[1] = cm;
+    freq[0] = analogRead(A0) / 3.0;
+    freq[1] = freq[0] * .5;
+    waveform1.frequency(freq[0]);
+    waveform2.frequency(freq[1]);
+  }
+
+
+  if (cm - prev[0] > 500) {
+    prev[0] = cm;
+
+    //Here we print out the usage of the audio library
+    // If we go over 90% processor usage or get near the value of memory blocks we set aside in the setup we'll have issues or crash.
+    // This pretty much all a copy paste
+    Serial.print("processor: ");
+    Serial.print(AudioProcessorUsageMax());
+    Serial.print("%    Memory: ");
+    Serial.print(AudioMemoryUsageMax());
+    Serial.println();
+    AudioProcessorUsageMaxReset(); //We need to reset these values so we get a real idea of what the audio code is doing rather than just peaking in every half a second
+    AudioMemoryUsageMaxReset();
+
+  }
+}
