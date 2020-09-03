742a617d6f8cbcdf8126ef3cf2599f7034e918b2
Dr. Bleep <drbleep@bleeplabs.com>
2020-09-03 15:32:33 -0500

    Class 5

--- Class_5/Class_5_-_Input_example/Class_5_-_Input_example.ino
+++ Class_5/Class_5_-_Input_example/Class_5_-_Input_example.ino
@@ -6,6 +6,12 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
 // GUItool: begin automatically generated code
 AudioAmplifier           amp1;           //xy=124,393
 AudioInputI2S            i2s2;           //xy=183,309
@@ -17,7 +23,7 @@ AudioOutputI2S           i2s1;           //xy=854,386
 AudioConnection          patchCord1(amp1, sine_fm1);
 AudioConnection          patchCord2(i2s2, 0, multiply1, 0);
 AudioConnection          patchCord3(i2s2, 0, mixer1, 2);
-AudioConnection          patchCord4(i2s2, 1, amp1, 0);
+AudioConnection          patchCord4(i2s2, 0, amp1, 0);
 AudioConnection          patchCord5(sine_fm1, 0, multiply1, 1);
 AudioConnection          patchCord6(sine_fm1, 0, mixer1, 1);
 AudioConnection          patchCord7(multiply1, 0, mixer1, 0);
@@ -27,7 +33,9 @@ AudioConnection          patchCord10(amp2, 0, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=510,293
 // GUItool: end automatically generated code
 
-#include <smooth.h> //smooth is located in a tab at the top instead of in this file. We just have to include it
+
+#include "smooth.h" //smooth is located in a tab at the top instead of in this file. We just have to include it
+// if the file to be included is in the same directory as the main .ino, use " ". Above we see <> this is for files in other directories.
 
 int pot[5];
 unsigned long cm;
@@ -48,9 +56,13 @@ void setup() {
   // See the hookup guide here
   sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
   //The volume of the input. 0 is low, 15 is high
-  sgtl5000_1.lineInLevel(7); //5 is default for line level
+  sgtl5000_1.lineInLevel(12); //5 is default for line level
 
 
+  //If you want to use a microhpne instead use these line. You can't use both at once
+  //sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
+  //sgtl5000_1.micGain(20); //number is from 0-63 dB of gain. Usually this gets set pretty high
+  
   //Headphone output volume.
   // For headphones it's pretty loud at .4
   sgtl5000_1.volume(0.25);
@@ -63,9 +75,9 @@ void setup() {
   amp2.gain(1);
 
   // gain.(channel from 0 to 3, gain from 0.0 to a large number)
-  mixer1.gain(0, .75);
-  mixer1.gain(1, 0);
-  mixer1.gain(2, .25);
+  mixer1.gain(0, .4);
+  mixer1.gain(1, .4);
+  mixer1.gain(2, .2);
   mixer1.gain(3, 0);
 
   //Then we do the stuff we've done before.
@@ -82,12 +94,13 @@ void loop() {
   // if we do this in the bottom of the loop, as in not in a timing if, it will respond much more quickly
   pot[0] = smooth(0, 35, analogRead(A1));
   pot[1] = smooth(1, 35, analogRead(A2));
-  pot[2] = smooth(1, 35, analogRead(A3));
+  pot[2] = smooth(2, 35, analogRead(A3));
 
-  if (cm - prev[0] > 500) {
-    prev[0] = cm;
-    sine_fm1.frequency(pot[0] / 4.0);
-    amp2.amplitude(pot[2] / 4095.0);
+  if (cm - prev[1] > 5) {
+    prev[1] = cm;
+    sine_fm1.frequency(pot[0] / 4.0); 
+    amp2.gain(pot[2] / 4095.0); //output volume
+    amp1.gain(pot[1] / 1000.0); //amount the incoming audio is FMing the oscillator
   }
 
   if (cm - prev[0] > 500) {
