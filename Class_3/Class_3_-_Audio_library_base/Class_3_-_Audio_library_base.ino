4f0865002a19e2c395e7e04e705b77f30c9f9530
Dr. Bleep <drbleep@bleeplabs.com>
2020-09-04 13:09:27 -0500

    Class 3

--- Class_3/Class_3_-_Audio_library_base/Class_3_-_Audio_library_base.ino
+++ Class_3/Class_3_-_Audio_library_base/Class_3_-_Audio_library_base.ino
@@ -1,4 +1,14 @@
-// input example
+// The block we copied from the tool is pasted at the top
+// design tool: https://www.pjrc.com/teensy/gui/
+
+// "#include" means add another file to our code
+// So far we've been copy and pasting things in but we can just tell our code to look
+// in a library for more functions and data
+// These are all necessary to get audio working but we don't need to do anything besides include them
+
+#include "smooth.h" //smooth is located in a tab at the top instead of in this file. We just have to include it
+// if the file to be included is in the same directory as the main .ino, use " ". Above we see <> this is for files in other directories.
+
 
 #include <Audio.h>
 #include <Wire.h>
@@ -7,88 +17,75 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioAmplifier           amp1;           //xy=124,393
-AudioInputI2S            i2s2;           //xy=183,309
-AudioSynthWaveformSineModulated sine_fm1;       //xy=207,473
-AudioEffectMultiply      multiply1;      //xy=328,389
-AudioMixer4              mixer1;         //xy=523,395
-AudioAmplifier           amp2;           //xy=680,388
-AudioOutputI2S           i2s1;           //xy=854,386
-AudioConnection          patchCord1(amp1, sine_fm1);
-AudioConnection          patchCord2(i2s2, 0, multiply1, 0);
-AudioConnection          patchCord3(i2s2, 0, mixer1, 2);
-AudioConnection          patchCord4(i2s2, 1, amp1, 0);
-AudioConnection          patchCord5(sine_fm1, 0, multiply1, 1);
-AudioConnection          patchCord6(sine_fm1, 0, mixer1, 1);
-AudioConnection          patchCord7(multiply1, 0, mixer1, 0);
-AudioConnection          patchCord8(mixer1, amp2);
-AudioConnection          patchCord9(amp2, 0, i2s1, 0);
-AudioConnection          patchCord10(amp2, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=510,293
+AudioSynthWaveform       waveform2;      //xy=167,267
+AudioSynthWaveform       waveform1;      //xy=175,215
+AudioMixer4              mixer1;         //xy=383,245
+AudioOutputI2S           i2s1;           //xy=591,244
+AudioConnection          patchCord1(waveform2, 0, mixer1, 1);
+AudioConnection          patchCord2(waveform1, 0, mixer1, 0);
+AudioConnection          patchCord3(mixer1, 0, i2s1, 0);
+AudioConnection          patchCord4(mixer1, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=484,355
 // GUItool: end automatically generated code
 
-#include <smooth.h> //smooth is located in a tab at the top instead of in this file. We just have to include it
+//You can edit the connections by hand but it's easier to do it in the audio library for now.
 
-int pot[5];
+
+//Then we have our variable declarations like before
 unsigned long cm;
 unsigned long prev[8];
 
 
 void setup() {
+  //there's a lot we need to do in setup now but some of it is just copy paste.
+  // This first group should only be done in setup
 
+  //How much RAM to set aside for the audio library to use.
   // The audio library uses blocks of a set size so this is not a percentage or kilobytes, just a kind of arbitrary number.
   // On our Teensy 3.2 we can go up to about 200 but that won't leave any RAM for anyone else.
   // It's usually the delay and reverb that hog it.
   AudioMemory(10);
 
-  //info for all of these "sgtl5000" functions are in the tool here https://www.pjrc.com/teensy/gui/?info=AudioControlSGTL5000
   sgtl5000_1.enable(); //Turn the adapter board on
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN); //Tell it what input we want to use. Not necessary yet but good to have
+  sgtl5000_1.lineInLevel(13); //The volume of the input. Again we'll get to this later
 
-  //Tell it what input we want to use. The line in is accessible on the top of the audio adapter
-  // See the hookup guide here
-  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
-  //The volume of the input. 0 is low, 15 is high
-  sgtl5000_1.lineInLevel(7); //5 is default for line level
-
-
-  //Headphone output volume.
+  //Output volume. Goes from 0.0 to 1.0 but a fully loud signal will clip over .8 or so.
   // For headphones it's pretty loud at .4
+  // There are lots of places we can change the final volume level. This one you set once and leave alone.
   sgtl5000_1.volume(0.25);
 
-
-  // begin(volume from 0.0-1.0 , frequency , shape of oscillator)
+  //This next group can be done anywhere in the code but we want to start things with these
+  // values and change some of them in the loop.
+  
+  //Notice we start by writing the object we want, then a period, then the function
+  // begin(volume from 0.0-1.0 , frequency , shape of oscillator) 
   // See the tool for more info https://www.pjrc.com/teensy/gui/?info=AudioSynthWaveform
-  sine_fm1.amplitude(1);
-  amp1.gain(.5);
-  amp2.gain(1);
+  waveform1.begin(1, 220.0, WAVEFORM_SINE);
+  waveform2.begin(1, 440.0, WAVEFORM_SINE);
 
+  //The mixer has four inputs we can change the volume of
   // gain.(channel from 0 to 3, gain from 0.0 to a large number)
-  mixer1.gain(0, .75);
-  mixer1.gain(1, 0);
-  mixer1.gain(2, .25);
+  // A gain of 1 means the output is the same as the input.
+  // .5 would be half volume and 2 would be double
+  // Since we have two oscillators coming in that are already "1" We should take them down by half so we don't clip.
+  // If you go over "1" The top or bottom of the wave is just slammed against a wall
+  mixer1.gain(0, .5);
+  mixer1.gain(1, .5);
+  mixer1.gain(2, 0);
   mixer1.gain(3, 0);
 
   //Then we do the stuff we've done before.
   analogWriteResolution(12); //PWM and A14 DAC output will be 0-4095. This has no effect on the 16 bit in/out of the audio adapter
   analogReadResolution(12); //AnalogReads will return 0-4095
-  analogReadAveraging(64);
+  analogReadAveraging(64); 
 
 }
 
 void loop() {
   cm = millis();
 
-  //smooth(channel, number of readings to average, input)
-  // if we do this in the bottom of the loop, as in not in a timing if, it will respond much more quickly
-  pot[0] = smooth(0, 35, analogRead(A1));
-  pot[1] = smooth(1, 35, analogRead(A2));
-  pot[2] = smooth(1, 35, analogRead(A3));
-
-  if (cm - prev[0] > 500) {
-    prev[0] = cm;
-    sine_fm1.frequency(pot[0] / 4.0);
-    amp2.amplitude(pot[2] / 4095.0);
-  }
+  //We don't have to do anything in the loop since the audio library will jut keep doing what we told it in the setup
 
   if (cm - prev[0] > 500) {
     prev[0] = cm;
